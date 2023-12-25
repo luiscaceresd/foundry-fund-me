ebe0a08ede78fe1e05f5ad89ca5c6abcdedd518a
luiscaceresd <luiscaceresd97@gmail.com>
2023-12-25 14:34:17 -0500

    refactoring data feeder

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.18;
 
 import { Test, console } from "forge-std/Test.sol";
 import { FundMe } from "../src/FundMe.sol";
+import { DeployFundMe } from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
   FundMe fundMe;
   function setUp() external {
-    fundMe = new FundMe();
+    DeployFundMe deployFundMe = new DeployFundMe();
+    fundMe = deployFundMe.run();
   }
 
   function testMinimumDollarIsFive() public {
@@ -15,7 +17,7 @@ contract FundMeTest is Test {
   }
 
   function testOwnerIsMsgSender() public {
-    assertEq(fundMe.i_owner(), address(this));
+    assertEq(fundMe.i_owner(), msg.sender);
   }
 
   function testPriceFeedVersionIsAccurate() public {
