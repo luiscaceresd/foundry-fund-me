20efcabae75266199890bc5a73f7be7f6bcb791b
luiscaceresd <luiscaceresd97@gmail.com>
2023-12-29 15:42:21 -0500

    optimizing withdraw gas

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -99,4 +99,29 @@ contract FundMeTest is Test {
       fundMe.getOwner().balance
     );
   }
+
+  function testWithdrawFromMultipleFundersCheaper () public funded {
+    // arrange
+    uint160 numberOfFunders = 10;
+    uint160 startingFunderIndex = 1; 
+    for(uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+      hoax(address(i), SEND_VALUE);
+      fundMe.fund{value: SEND_VALUE}();
+    }
+    
+    uint256 startingOwnerBalance = fundMe.getOwner().balance;
+    uint256 startingFundMeBalance = address(fundMe).balance;
+
+    // act
+    vm.startPrank(fundMe.getOwner());
+    fundMe.cheaperWithdraw();
+    vm.stopPrank();
+
+    //assert
+    assert(address(fundMe).balance == 0);
+    assert(
+      startingFundMeBalance + startingOwnerBalance == 
+      fundMe.getOwner().balance
+    );
+  }
 }
