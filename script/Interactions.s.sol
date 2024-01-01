e389a579fae17199dea68127684700670d485816
luiscaceresd <luiscaceresd97@gmail.com>
2023-12-31 19:35:33 -0500

    adding withdrawFundMe and tests

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import { Script, console } from "forge-std/Script.sol";
 import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
@@ -20,10 +20,26 @@ contract FundFundMe is Script {
       "FundMe", 
       block.chainid
     );
+    vm.startBroadcast();
     fundFundMe(mostRecentlyDeployed);
+    vm.stopBroadcast();
   }
 }
 
 contract WithdrawFundMe is Script {
+  function withdrawFundMe(address mostRecentlyDeployed) public {
+    vm.startBroadcast();  
+    FundMe(payable(mostRecentlyDeployed)).withdraw();
+    vm.stopBroadcast(); 
+  }
 
+  function run() external {
+    address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+      "FundMe", 
+      block.chainid
+    );
+    vm.startBroadcast();
+    WithdrawFundMe(mostRecentlyDeployed);
+    vm.stopBroadcast();
+  }
 }
